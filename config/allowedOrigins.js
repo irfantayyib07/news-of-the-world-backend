65718e97b22f47098613408e580c3e258b3ed05e
Irfan <irfantayyib07@gmail.com>
2024-02-28 19:07:19 +0500

    CORS issue

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
  "http://localhost:3000/",
  "http://localhost:3000",
  "http://127.0.0.1:5500/",
- "http://newsoftheworld.vercel.app/",
+ "https://newsoftheworld.vercel.app/",
+ "https://newsoftheworld.vercel.app",
 ];
 
 module.exports = allowedOrigins
